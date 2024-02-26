200008c6ec75ab7859c12352b2ccdb7b8b5340ec
rimapaulione <rimapaulione@gmail.com>
2024-02-26 21:44:51 +0200

    address update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const URL = " http://api.openweathermap.org";
+export const URL = " https://api.openweathermap.org";
 
 export const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-export const BASE_URL = `http://api.openweathermap.org/data/2.5/weather`;
+export const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
-export const FORECAST_URL = `http://api.openweathermap.org/data/2.5/forecast`;
+export const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast`;
