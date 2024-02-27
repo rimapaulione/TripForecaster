d922f9ac57d17d1763a3a54ce5c85a9ce239ab9e
rimapaulione <rimapaulione@gmail.com>
2024-02-27 13:01:00 +0200

    edit navigation

--- src/ui/PageNav.jsx
+++ src/ui/PageNav.jsx
@@ -1,8 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 import styles from "./PageNav.module.css";
+import BackButton from "./BackButton";
 
 function PageNav() {
+  const url = useLocation();
+
   return (
     <nav className={styles.nav}>
       <Logo />
@@ -13,9 +16,13 @@ function PageNav() {
         </li>
 
         <li>
-          <NavLink to="/" className={styles.ctaLink}>
-            Login
-          </NavLink>
+          {url.pathname === "/forecast" ? (
+            <BackButton></BackButton>
+          ) : (
+            <NavLink to="/" className={styles.ctaLink}>
+              Login
+            </NavLink>
+          )}
         </li>
       </ul>
     </nav>
